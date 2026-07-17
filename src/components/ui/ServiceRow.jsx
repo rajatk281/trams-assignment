5b1fdb3466b6c96c9b37656f6788c305193902f1
Rajat Kumar <rajat28012006@gmail.com>
2026-07-17 10:58:44 +0530

    services section completed

---
+++ src/components/ui/ServiceRow.jsx
@@ -0,0 +1,38 @@
+import { MoveRight } from 'lucide-react'
+
+const serviceItems = [{
+    id: 1,
+    desc: "Office of multiple interest content",
+    heading: "Colaborative & partnership",
+},
+{
+    id: 2,
+    desc: "The hanger US Air force digital experimental",
+    heading: "We talk about our weight",
+},
+{
+    id: 3,
+    desc: "Delta faucet content, social, digital",
+    heading: "Piloting digital confidence",
+}
+]
+
+const ServiceRow = () => {
+    return (
+        <div>
+            <hr />
+            {
+                serviceItems.map((items) => (
+                    <div key={items.id} className='flex justify-between items-center py-6 border-b'>
+                        <p className=' max-w-50'>{items.desc}</p>
+                        <h3>{items.heading}</h3>
+                        <MoveRight />
+                    </div>
+
+                ))
+            }
+        </div>
+    )
+}
+
+export default ServiceRow
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import ServiceRow from '../components/ui/ServiceRow'
+import { LinePattern, Texthighlight } from '../components/common/DecorativeShape'
 
 const Services = () => {
-  return (
-    <div>Services</div>
-  )
+    return (
+        <div className=' px-18 my-18 relative'>
+            <h2 className='pb-12 z-10'>What we <Texthighlight text="can" color="green" height={85} width={120} left={-10} />{" "} <br /> offer you!</h2>
+            <ServiceRow />
+            <div className="absolute top-25 left-19 z-[-1] scale-40 origin-top-left ">
+                <LinePattern  />
+            </div>
+        </div>
+    )
 }
 
 export default Services
